7fec5db104d722f2b03f4902ab88bd4ed37db8ff
piazzai <piazzai@pm.me>
2022-09-06 02:35:48 +0200

    Add particles.js

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    particlesJS.load('particles-home', '/assets/json/particles-home.json', function() {
+      console.log('particles.js loaded - callback');
+    });
+    particlesJS.load('particles-404', '/assets/json/particles-404.json', function() {
+      console.log('particles.js loaded - callback');
+    });
+  },
+  false,
+);
