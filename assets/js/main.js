2c2a1c9bf2fd157d2e38a83006778ad45347419b
piazzai <piazzai@pm.me>
2023-03-03 11:03:54 +0100

    Format file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    particlesJS.load('particles-home', '/assets/particles-home.json', function() {
-      console.log('particles.js loaded - callback');
-    });
-    particlesJS.load('particles-404', '/assets/particles-404.json', function() {
-      console.log('particles.js loaded - callback');
-    });
+document.addEventListener(
+  "DOMContentLoaded",
+  (event) => {
+    particlesJS.load(
+      "particles-home",
+      "/assets/particles-home.json",
+      function () {
+        console.log("particles.js loaded - callback");
+      }
+    );
+    particlesJS.load(
+      "particles-404",
+      "/assets/particles-404.json",
+      function () {
+        console.log("particles.js loaded - callback");
+      }
+    );
   },
-  false,
+  false
 );
