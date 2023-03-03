2d1e983d1c08574a169c84f3d2eceee1673b451e
piazzai <piazzai@pm.me>
2023-03-03 11:03:31 +0100

    Update json paths

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    particlesJS.load('particles-home', '/assets/json/particles-home.json', function() {
+    particlesJS.load('particles-home', '/assets/particles-home.json', function() {
       console.log('particles.js loaded - callback');
     });
-    particlesJS.load('particles-404', '/assets/json/particles-404.json', function() {
+    particlesJS.load('particles-404', '/assets/particles-404.json', function() {
       console.log('particles.js loaded - callback');
     });
   },
