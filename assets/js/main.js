5d916c843c4e77508a1950e9895eade6d526f517
piazzai <piazzai@pm.me>
2023-03-03 15:34:10 +0100

    Merge particles config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,9 @@
 document.addEventListener(
   "DOMContentLoaded",
   (event) => {
-    particlesJS.load(
-      "particles-home",
-      "/assets/particles-home.json",
-      function () {
-        console.log("particles.js loaded - callback");
-      }
-    );
-    particlesJS.load(
-      "particles-404",
-      "/assets/particles-404.json",
-      function () {
-        console.log("particles.js loaded - callback");
-      }
-    );
+    particlesJS.load("particles-js", "/assets/particles.json", function () {
+      console.log("particles.js loaded - callback");
+    });
   },
   false
 );
